49cb45200f055ee790ee45ec6ccdad7ee37b3f71
Natalie Lane <nlane@udel.edu>
2022-12-10 11:58:40 -0500

    day10

---
+++ days/day10/solution.py
@@ -0,0 +1,47 @@
+from days import utils
+
+
+def update_cursor(screen_i, screen_j):
+    if screen_j in [39, 79, 119, 159, 199, 239]:
+        screen_j = 0
+        screen_i += 1
+    else:
+        screen_j += 1
+    return (screen_i, screen_j)
+
+
+def main():
+    instructions = utils.open_file(10).split("\n")
+    notable_cycles = [20, 60, 100, 140, 180, 220]
+    notable_cycle_signals = []
+    cycle = 1
+    x = 1
+    screen = [["" for j in range(40)] for i in range(6)]
+    screen_i = 0
+    screen_j = 0
+    for instruction in instructions:
+        if screen_i == 6:
+            break
+        instruction_type = instruction[:4]
+        if cycle in notable_cycles:
+            notable_cycle_signals.append(x * cycle)
+        if screen_j in [x - 1, x, x + 1]:
+            screen[screen_i][screen_j] = "#"
+        else:
+            screen[screen_i][screen_j] = "."
+        screen_i, screen_j = update_cursor(screen_i, screen_j)
+        if instruction_type == "addx":
+            value = instruction.split(" ")[1]
+            cycle += 1
+            if screen_j in [x - 1, x, x + 1]:
+                screen[screen_i][screen_j] = "#"
+            else:
+                screen[screen_i][screen_j] = "."
+            screen_i, screen_j = update_cursor(screen_i, screen_j)
+            if cycle in notable_cycles:
+                notable_cycle_signals.append(x * cycle)
+            x += int(value)
+        cycle += 1
+    print("Part 1: ", sum(notable_cycle_signals))
+    for line in screen:
+        print("".join(line))
